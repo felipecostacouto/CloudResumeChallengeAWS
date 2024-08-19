e63ae673dc10bf5d036df63f0b7756182ee4d802
felipecostacouto <felipe.costacouto19@gmail.com>
2024-08-19 20:57:49 -0300

    Fix test files py 4

--- test_lambda/test_lambda.py
+++ test_lambda/test_lambda.py
@@ -2,21 +2,24 @@ import pytest
 from unittest.mock import patch, MagicMock
 from botocore.exceptions import ClientError
 
-@pytest.fixture(autouse=True)
-def mock_boto3_resource():
-    with patch('backEnd.lambda_handler.boto3.resource') as mock_resource:
-        yield mock_resource
-
 from backEnd.lambda_handler import lambda_handler
+
+@pytest.fixture(autouse=True)
+def mock_aws_credentials():
+    """Mocked AWS Credentials and region for tests."""
+    with patch.dict('os.environ', {
+        'AWS_ACCESS_KEY_ID': 'testing',
+        'AWS_SECRET_ACCESS_KEY': 'testing',
+        'AWS_DEFAULT_REGION': 'us-east-1'
+    }):
+        yield
 
 @pytest.fixture
 def mock_dynamodb_table():
-
-    with patch('backEnd.dynamodb.Table') as mock_table:
+    with patch('backEnd.lambda_handler.dynamodb.Table') as mock_table:
         yield mock_table
 
 def test_lambda_handler_success(mock_dynamodb_table):
-
     mock_table_instance = mock_dynamodb_table.return_value
     mock_table_instance.get_item.return_value = {'Item': {'views': 5}}
     
@@ -24,12 +27,10 @@ def test_lambda_handler_success(mock_dynamodb_table):
     context = {}
     response = lambda_handler(event, context)
     
-    # Verify that the response has the correct view count
     assert response['statusCode'] == 200
     assert response['body'] == 6  # Since the view count should be incremented by 1
 
 def test_lambda_handler_dynamodb_error(mock_dynamodb_table):
-    
     mock_table_instance = mock_dynamodb_table.return_value
     mock_table_instance.get_item.side_effect = ClientError(
         error_response={'Error': {'Code': 'SomeError', 'Message': 'An error occurred'}},
@@ -40,6 +41,5 @@ def test_lambda_handler_dynamodb_error(mock_dynamodb_table):
     context = {}
     response = lambda_handler(event, context)
 
-    # Verify that the response indicates an error
     assert response['statusCode'] == 500
     assert "error" in response['body']  # Check if an error message is in the body
