f0904d1ce29a8d6480f06f3f8f9b4570eba11bf8
felipecostacouto <felipe.costacouto19@gmail.com>
2024-09-07 15:57:18 -0300

    CORS fix 1.8

--- terraform/modules/api_gateway/outputs.tf
+++ terraform/modules/api_gateway/outputs.tf
@@ -1,4 +1,4 @@
 output "api_url" {
   description = "The URL of the deployed API"
-  value       = "${aws_api_gateway_deployment.deployment.invoke_url}${aws_api_gateway_stage.prod_stage.stage_name}/${aws_api_gateway_resource.resource.path_part}"
+  value       = "${aws_api_gateway_deployment.deployment.invoke_url}${aws_api_gateway_stage.rest_api_stage.stage_name}/${aws_api_gateway_resource.resource.path_part}"
 }
